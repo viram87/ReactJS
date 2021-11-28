06ccfc1f61989660f4f12f5a5bb11f94a46c802f
viram choksi <viramchoksi77166@gmail.com>
2021-11-28 23:29:32 +0530

    day3

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,21 +21,49 @@ export default function TextForm(props) {
     const handleonchange = (event)=> {
         console.log("on change");
         setText(event.target.value);
-        
-        
+               
+    }
+
+    const handleClear = ()=> {
+        let clear = "";
+        setText(clear);
+
     }
                        
-        const [Text, setText] = useState('enter text here');
+        const [Text, setText] = useState('');
         return (
         <div>
         <div className="container">
         <div className="mb-3">
             <label htmlFor="myBox" className="form-label">{props.heading}</label>
-            <textarea className="form-control" value={Text} onChange={handleonchange} id="myBox" rows="3"></textarea>
+            <textarea className="form-control" placeholder = "enter text here" value={Text} onChange={handleonchange} id="myBox" rows="3"></textarea>
+            <br />
             <button className="btn btn-primary" onClick={handleUpclick}>Convert to uppercase</button>
-            <button className="btn btn-primart" onClick={handleLoclick}>Convert to lower case</button>
+           
+            <button className="btn btn-primary mx-2" onClick={handleLoclick}>Convert to lower case</button>
+
+            <button className="btn btn-primary" onClick={handleClear}>Clear</button>
         </div>
         </div>
+
+        <div className="container">
+            
+             <h2>Text summary</h2>   
+
+            <p>
+                length of characters is {Text.length} and length of words is {Text.split(" ").length}
+            </p>
+            <p>
+            {0.008*Text.split(" ").length} Minutes to read
+            </p>
+
+            <h2>Preview</h2>
+            <p>{Text}</p>
+
         </div>
+        </div>
+
+
+        
         )    
-        }
+        }   
